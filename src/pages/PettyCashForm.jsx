78b1713320925eeba5460649e6f2523269f37e7e
David Kunle-Dauda <davidkunledauda2003@gmail.com>
2023-08-27 20:55:54 +0100

    temporary retrace for demo purpose

--- src/pages/PettyCashForm.jsx
+++ src/pages/PettyCashForm.jsx
@@ -37,35 +37,33 @@ export function PettyCashForm() {
 
   const onSubmit = async (data) => {
     // Calculate the total amount
-    console.log("payload", data);
-    const formData = new FormData();
-    Object.keys(data).forEach((key) => {
-      if (["items", "accountDetails", "attachment"].includes(key)) return;
-      let value = data[key];
-      formData.append(key, value);
-    });
-    formData.append(
-      "accountDetails[accountName]",
-      data.accountDetails.accountName
-    );
-    formData.append("accountDetails[number]", data.accountDetails.number);
-    formData.append("accountDetails[bank_name]", data.accountDetails.bank_name);
 
-    const total = data.items.reduce((accumulator, item, index) => {
-      formData.append(`items[${index}][amount]`, item.amount);
-      formData.append(`items[${index}][name]`, item.name);
-      return accumulator + parseInt(item.amount);
-    }, 0);
-    formData.append("total", total);
+    const total = data.items.reduce(
+      (accumulator, item) => accumulator + parseInt(item.amount),
+      0
+    );
     setTotal(total);
-    const attachment = data.attachment[0];
-    formData.append("attachment", attachment);
+
     console.log(data);
+
+    // const response = await axios.post(
+    //   "http://localhost:3000/create-request",
+    //   { ...data, total }
+    // );
+
+    // const formData = new FormData();
+    // for (const key in form) {
+    //   formData.append(key, form[key]);
+    // }
+
     httpClient
-      .post("/create-request", formData)
+      .post("/create-request", {
+        ...data,
+        total,
+        // formData,
+      })
       .then((res) => {
         setForm(res.data);
-        console.log(res.data);
         setShowForm(false);
       })
       .catch((err) => {
@@ -87,7 +85,6 @@ export function PettyCashForm() {
   const handleMenuToggle = () => {
     setShowMenu(!showMenu);
   };
-
   return (
     <>
       <NavBar />
@@ -112,6 +109,123 @@ export function PettyCashForm() {
     </>
   );
 }
+
+
+//PREVIOUS COMMIT
+// // import logo from "./logo.svg";
+// import { useState, useContext } from "react";
+// import PetiCashForm from "../components/form.jsx";
+// import Display from "../components/displaypage/display.jsx";
+// import { Link, Route, Routes } from "react-router-dom";
+// import { AuthContext } from "./useContext/context.js";
+// import axios from "axios";
+// import httpClient from "../hooks/server.js";
+// import { BsPersonFill } from "react-icons/bs";
+// import NavBar from "../components/NavBar.jsx";
+
+// export function PettyCashForm() {
+//   const initialState = {
+//     name: "",
+//     date: "",
+//     accountDetails: {
+//       number: "",
+//       bank: "",
+//       accountName: "",
+//     },
+//     authorizedBy: "",
+//     items: [],
+//     attachment: "",
+//   };
+//   const [form, setForm] = useState(initialState);
+//   const [itemForm, setItemForm] = useState({ name: "", amount: 0 });
+//   const [showForm, setShowForm] = useState(true);
+//   const [total, setTotal] = useState();
+//   const [showModal, setShowModal] = useState(false);
+//   const { user } = useContext(AuthContext);
+
+//   const handleReturn = () => {
+//     setForm(initialState);
+//     setTotal(null);
+//     setShowForm(true);
+//   };
+
+//   const onSubmit = async (data) => {
+//     // Calculate the total amount
+//     console.log("payload", data);
+//     const formData = new FormData();
+//     Object.keys(data).forEach((key) => {
+//       if (["items", "accountDetails", "attachment"].includes(key)) return;
+//       let value = data[key];
+//       formData.append(key, value);
+//     });
+//     formData.append(
+//       "accountDetails[accountName]",
+//       data.accountDetails.accountName
+//     );
+//     formData.append("accountDetails[number]", data.accountDetails.number);
+//     formData.append("accountDetails[bank_name]", data.accountDetails.bank_name);
+
+//     const total = data.items.reduce((accumulator, item, index) => {
+//       formData.append(`items[${index}][amount]`, item.amount);
+//       formData.append(`items[${index}][name]`, item.name);
+//       return accumulator + parseInt(item.amount);
+//     }, 0);
+//     formData.append("total", total);
+//     setTotal(total);
+//     const attachment = data.attachment[0];
+//     formData.append("attachment", attachment);
+//     console.log(data);
+//     httpClient
+//       .post("/create-request", formData)
+//       .then((res) => {
+//         setForm(res.data);
+//         console.log(res.data);
+//         setShowForm(false);
+//       })
+//       .catch((err) => {
+//         console.log("error", err);
+//       });
+//   };
+
+//   const handleModal = () => {
+//     setShowModal(true);
+//   };
+
+//   const removeUser = () => {
+//     setShowModal(false);
+//     window.localStorage.clear();
+//     window.location.href = "/login";
+//   };
+
+//   const [showMenu, setShowMenu] = useState(false);
+//   const handleMenuToggle = () => {
+//     setShowMenu(!showMenu);
+//   };
+
+//   return (
+//     <>
+//       <NavBar />
+//       <div className="">
+//         {showForm && (
+//           <PetiCashForm
+//             submit={onSubmit}
+//             setForm={setForm}
+//             form={form}
+//             itemForm={itemForm}
+//             setItemForm={setItemForm}
+//           />
+//         )}
+//         {!showForm && (
+//           <Display
+//             form={form?.newPettyCashRequest}
+//             total={total}
+//             onReturn={handleReturn}
+//           />
+//         )}
+//       </div>
+//     </>
+//   );
+// }
 
 // httpClient
 //   .post("/create-request", {
